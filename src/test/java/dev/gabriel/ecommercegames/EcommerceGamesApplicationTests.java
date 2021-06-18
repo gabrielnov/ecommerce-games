8cc8638cf6b7e92ec87d9c40a2530b159fb428cc
Gabriel <gc.novaes@outlook.com>
2021-06-18 14:41:23 -0300

    commit inicial

---
+++ src/test/java/dev/gabriel/ecommercegames/EcommerceGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabriel.ecommercegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
